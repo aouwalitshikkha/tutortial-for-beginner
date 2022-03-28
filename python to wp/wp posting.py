4b80679f3e68313da44be1dab0e35f9f4357e7d7
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-03-28 09:34:33 +0600

    Serialize the wp posting data

--- python to wp/wp posting.py
+++ python to wp/wp posting.py
@@ -3,17 +3,26 @@ import requests
 
 
 
-#Website information
+#Authentication information
 user = 'Your User Name'  # the user in which the auth. token is given
 website_url = 'https://example.com' #Your website url
+pythonapp = 'Authentication Token Genereted from Wp'  # paste here your auth. token
+url = website_url +'/wp-json/wp/v2'  # the url of the wp access location
+
+
+# header Generating 
+token = base64.standard_b64encode((user + ':' + pythonapp).encode('utf-8')) # we have to encode the usr and pw
+headers = {'Authorization': 'Basic ' + token.decode('utf-8')}
+
+
+#Wordpress Posting information
 wp_title = 'Your Post Tittle' # Your Post Title 
 content = 'Post Body Text' # Your Post content here
 slug = 'post-url' #Your Post URL
-pythonapp = 'Authentication Token Genereted from Wp'  # paste here your auth. token
 
-url = website_url +'/wp-json/wp/v2'  # the url of the wp access location
-token = base64.standard_b64encode((user + ':' + pythonapp).encode('utf-8')) # we have to encode the usr and pw
-headers = {'Authorization': 'Basic ' + token.decode('utf-8')}
+
+
+
 
 post = {'title': wp_title,
         'slug': slug,
